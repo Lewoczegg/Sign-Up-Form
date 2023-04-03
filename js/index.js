24cf7c92e42a5771be4e103a6c5c46479fcac51b
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-04-03 20:07:44 +0200

    Add form validation messages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,39 @@
+const form = document.getElementById('myForm');
 const inputRows = document.querySelectorAll('.input-row');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', event => {
+    console.log(event);
+    event.preventDefault();
+
+    let isValid = true;
+
+    inputRows.forEach(inputRow=> {
+        const input = inputRow.querySelector('input');
+        const errorMessage = inputRow.querySelector('.error-message');
+
+        if(!input.checkValidity()) {
+            inputRow.classList.add('invalid');
+            errorMessage.style.display = 'block';
+            isValid = false;
+        } else {
+            inputRow.classList.remove('invalid');
+            errorMessage.style.display = 'none';
+        }
+
+        input.addEventListener("input", () => {
+            if (input.checkValidity()) {
+              inputRow.classList.remove("invalid");
+              errorMessage.style.display = "none";
+            }
+        });
+    });
+
+
+    if(isValid) {
+        form.submit();
+    }
+});
 
 inputRows.forEach(inputRow => {
     const input = inputRow.querySelector('input');
